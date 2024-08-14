a7e4a0963f01c229f550bf35842073cec3cb7a02
saikumar <sai680513@gmail.com>
2024-08-14 13:43:06 +0530

    extract embeddings

---
+++ utils/emdeddings.py
@@ -0,0 +1,11 @@
+from keras_facenet import FaceNet
+import numpy as np
+embedder = FaceNet()
+
+def get_embedding(face_img):
+    face_img = face_img.astype('float32') # 3D(160x160x3)
+    face_img = np.expand_dims(face_img, axis=0) 
+    # 4D (Nonex160x160x3)
+    yhat= embedder.embeddings(face_img)
+    return yhat[0] # 512D image (1x1x512)
+
